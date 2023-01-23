306e8c85ec138316f1dadfff3763bd9ad49792c2
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-23 15:18:11 +0500

    Initial commit

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var ButtonCollection: [UIButton]!
+    
+    @IBOutlet weak var touchLabel: UILabel!
+    
+    @IBAction func ButtonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = ButtonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojis[buttonIndex], button: sender)
+        }
+    }
+    
+    let emojis = ["🦊", "🐰", "🦊", "🐰"]
+
+    var touches = 0 {
+        didSet{
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
+    private func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 
-
+    
 }
 
